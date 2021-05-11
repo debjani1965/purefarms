053ba1e07faf93b164bbe97d59c8d74300b87bec
Debjani <shellysg.sengupta@gmail.com>
2021-05-11 09:33:12 +0530

    Add env file

--- db/db.connection.js
+++ db/db.connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost:27017/purefarms', {useNewUrlParser: true, useUnifiedTopology: true});
-const dbUrl = "mongodb+srv://shelly:test123@learnmongo.05bfw.mongodb.net/purefarms?retryWrites=true&w=majority";
+const dbUrl = process.env.DB_URL;
 mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => {
           console.log("Connected to db")
